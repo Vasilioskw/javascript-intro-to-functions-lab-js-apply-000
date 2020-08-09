117784d957da5976b8e4cf5dc5cf4f539a53c36a
Vasilios Kambouras <vasilioskw@gmail.com>
2020-08-09 17:25:55 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -10,7 +10,9 @@ describe('shout(string)', function() {
     expect(shout('hello')).toEqual('HELLO')
   })
 })
-
+function shout (aWord) {
+  return aWord.toUpperCase( );
+}
 describe('whisper(string)', function() {
   it('receives one argument and returns it in all lowercase', function() {
     expect(whisper('HELLO')).toEqual('hello')
