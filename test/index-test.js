aa631abd3fdebeaf6279c221109e71c0fa894535
Vasilios Kambouras <vasilioskw@gmail.com>
2020-08-09 17:56:54 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -41,4 +41,8 @@ describe('sayHiToGrandma(string)', function() {
   it('returns "I love you, too." if `string` is "I love you, Grandma."`', function() {
     expect(sayHiToGrandma("I love you, Grandma.")).toEqual("I love you, too.")
   })
+  
+  console.log(string.toUpperCase()
+  if 
+  
 })
