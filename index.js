0889a9aa39a84e231a7e5b7617477bbcb516ca20
Vasilios Kambouras <vasilioskw@gmail.com>
2020-08-09 20:00:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ if (string === string.toLowerCase()) {
     return "I can't hear you!";
   }
   if (string === string.toUpperCase()) {
-  return 'YES INDEED';
+  return 'YES INDEED!';
   }
  if (string === "I love you, Grandma.") {
   return 'I love you, too.';
