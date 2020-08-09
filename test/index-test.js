57cd56ba9bbb5eed00d807134eef98b4f47069a5
Vasilios Kambouras <vasilioskw@gmail.com>
2020-08-09 15:34:31 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -4,7 +4,7 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase()
 }
-
+function sayHiToGrandma()
 describe('shout(string)', function() {
   it('receives one argument and returns it in all caps', function() {
     expect(shout('hello')).toEqual('HELLO')
