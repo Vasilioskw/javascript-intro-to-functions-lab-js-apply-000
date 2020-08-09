193f69dfba26b7cf57aaba8916e2330ae0a9ecdb
Vasilios Kambouras <vasilioskw@gmail.com>
2020-08-09 18:01:39 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -41,7 +41,7 @@ describe('sayHiToGrandma(string)', function() {
   it('returns "I love you, too." if `string` is "I love you, Grandma."`', function() {
     expect(sayHiToGrandma("I love you, Grandma.")).toEqual("I love you, too.")
     
-    If the string is ALREADY all lowercase, return "I can't hear you!"
+    console.log() toLowerCase return "I can't hear you!"
 
 If the string is ALREADY all uppercase, return "YES INDEED!"
 
