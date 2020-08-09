d96de51461928885f5991c6e243c9f12f763a7bf
Vasilios Kambouras <vasilioskw@gmail.com>
2020-08-09 18:16:21 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -40,9 +40,33 @@ describe('sayHiToGrandma(string)', function() {
 
   it('returns "I love you, too." if `string` is "I love you, Grandma."`', function() {
     expect(sayHiToGrandma("I love you, Grandma.")).toEqual("I love you, too.")
-    
-    console.log(string.toLowerCase()) If the string is ALREADY all lowercase, return "I can't hear you!"
-
   })
-  
 })
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!"
+  }
+
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  }
+
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
