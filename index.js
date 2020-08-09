16415bbe6f2b4f898d455adf3a0cfa5ac9614a33
Vasilios Kambouras <vasilioskw@gmail.com>
2020-08-09 18:58:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function logWhisper (string) {
   console.log(string.toLowerCase())
 }
 
-function sayHiToGrandma(string) {
+function logsayHiToGrandma(string) {
   console.log(string.toMixedCase())
 }
