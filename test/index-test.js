87cbfb15fa18ea2076f43afeff9392472756ef50
Vasilios Kambouras <vasilioskw@gmail.com>
2020-08-09 15:31:28 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -5,8 +5,11 @@ function whisper(string) {
   return string.toLowerCase()
 }
 
-function whisper(string) {
-  return string.toLowerCase()
+function sayHiToGrandma(string)
+  it string.toLowerCase()
+  
+  it('returns "I can\'t hear you!" if `string` is lowercase', function() {
+    expect(sayHiToGrandma('hello')).toEqual("I can't hear you!")
 }
 
 describe('shout(string)', function() {
