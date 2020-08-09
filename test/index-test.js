89b09cc919c59159fd9cc7d4af21a92563c8d697
Vasilios Kambouras <vasilioskw@gmail.com>
2020-08-09 17:31:40 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -1,29 +1,10 @@
-function shout(string) {
-  return string.toUpperCase()
-}
-function whisper(string) {
-  return string.toLowerCase()
-}
-
 describe('shout(string)', function() {
-  it('receives one argument and returns it in all caps', function() {
-    expect(shout('hello')).toEqual('HELLO')
-  })
+it('receives one argument and returns it in all caps', function() {
+expect(shout('hello')).toEqual('HELLO')
 })
-
 function shout (aWord) {
   return aWord.toUpperCase( );
 }
-describe('whisper(string)', function() {
-  it('receives one argument and returns it in all lowercase', function() {
-    expect(whisper('HELLO')).toEqual('hello')
-    
-    function whisper (aWord) {
-  return aWord.toLowerCase();
-}
-
-  })
-})
 
 describe('logShout(string)', function() {
   it('takes a string argument and logs it in all caps using console.log()', function() {
